1a81a022c0dee5c244ba991e506f2a1d3d94ad47
makarovas <makarofalex@gmail.com>
2024-03-17 18:08:16 +0300

    add tests

--- src/__ tests __/useBolean.test.ts
+++ src/__ tests __/useBolean.test.ts
@@ -9,4 +9,22 @@ describe('useBoolean array', () => {
     act(() => actions.setTrue());
     expect(result.current[0]).toBe(true);
   });
+
+  it('should set false', () => {
+    const { result } = renderHook(() => useBoolean(true));
+    const [, actions] = result.current;
+    expect(result.current[0]).toBe(true);
+    act(() => actions.setFalse());
+    expect(result.current[0]).toBe(false);
+  });
+
+  it('should be toggle state', () => {
+    const { result } = renderHook(() => useBoolean(true));
+    const [, actions] = result.current;
+    expect(result.current[0]).toBe(true);
+    act(() => actions.toggle());
+    expect(result.current[0]).toBe(false);
+    act(() => actions.toggle());
+    expect(result.current[0]).toBe(true);
+  });
 });
