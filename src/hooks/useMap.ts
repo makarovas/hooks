d4894d1262cf8fccd0062ae6cc98536c1e381682
makarovas <makarofalex@gmail.com>
2024-03-17 20:39:58 +0300

    add get to useMap

--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -3,6 +3,7 @@ import { useCallback, useMemo, useState } from 'react';
 export type MapOrEntries<K, V> = Map<K, V> | [K, V][];
 export type UseMapActions<K, V> = {
   delete: (keyToDelete: K) => void;
+  get: (k: K) => void;
   set: (k: K, v: V) => void;
   clear: Map<K, V>['clear'];
   init: (pairsOrMap: MapOrEntries<K, V>) => void;
@@ -13,7 +14,7 @@ export type UseMap<K, V> = [MapOrEntries<K, V>, UseMapActions<K, V>];
 export function useMap<K, V>(
   initValues: MapOrEntries<K, V> = new Map()
 ): UseMap<K, V> {
-  const [map, setMap] = useState(
+  const [map, setMap] = useState<Map<K, V> | [K, V][]>(
     Array.isArray(initValues) ? new Map(initValues) : initValues
   );
 
@@ -23,6 +24,13 @@ export function useMap<K, V>(
       return copy.set(key, value);
     });
   }, []);
+
+  const get = useCallback(
+    (key: K) => {
+      return map instanceof Map ? map.get(key) : undefined;
+    },
+    [map]
+  );
 
   const deleteByKey = useCallback((key: K) => {
     setMap((_map) => {
@@ -44,11 +52,12 @@ export function useMap<K, V>(
   const actions = useMemo(
     () => ({
       set,
+      get,
       delete: deleteByKey,
       clear,
       init,
     }),
-    [clear, deleteByKey, init, set]
+    [clear, deleteByKey, get, init, set]
   );
 
   return [map, actions];
